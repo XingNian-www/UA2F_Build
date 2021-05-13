f4b3461e5d37d579767dda696bdccaddcb58d426
pxw139 <44593434+pxw139@users.noreply.github.com>
2021-05-13 11:45:22 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -3,6 +3,10 @@
 # 添加SSRP+
 echo "src-git helloworld https://github.com/fw876/helloworld.git" >> feeds.conf.default
 
+# 添加OpenClash
+git clone https://github.com/vernesong/OpenClash.git
+cp -rf OpenClash/luci-app-openclash package/luci-app-openclash
+
 #添加UA2F
 git clone https://github.com/Zxilly/UA2F.git package/UA2F
 

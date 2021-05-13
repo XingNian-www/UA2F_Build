0892db28d240f7ad7009a588066ef438d7afae97
XingNian-www <44593434+XingNian-www@users.noreply.github.com>
2021-05-13 17:47:44 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -7,30 +7,11 @@ echo "src-git helloworld https://github.com/fw876/helloworld.git" >> feeds.conf.
 git clone https://github.com/vernesong/OpenClash.git
 cp -rf OpenClash/luci-app-openclash package/luci-app-openclash
 
-#添加UA2F
+# 添加UA2F
 git clone https://github.com/Zxilly/UA2F.git package/UA2F
 
-#更新queue
+# 更新queue
 git clone https://github.com/openwrt/packages
 rm -rf package/libs/libnetfilter-queue
+# 本地用cp命令可以方便git pull
 cp -rf packages/libs/libnetfilter-queue package/libs/
-
-# 修改内核设置,不直接全部修改原因是看起来不舒服
-echo "CONFIG_IP_SET=y" >> target/linux/x86/config-5.4
-echo "CONFIG_IP_SET_HASH_IPPORT=y" >> target/linux/x86/config-5.4
-echo "CONFIG_IP_SET_MAX=256" >> target/linux/x86/config-5.4
-echo "CONFIG_NETFILTER=y" >> target/linux/x86/config-5.4
-echo "CONFIG_NETFILTER_FAMILY_ARP=y" >> target/linux/x86/config-5.4
-echo "CONFIG_NETFILTER_FAMILY_BRIDGE=y" >> target/linux/x86/config-5.4
-echo "CONFIG_NETFILTER_NETLINK=y" >> target/linux/x86/config-5.4
-echo "CONFIG_NETFILTER_NETLINK_GLUE_CT=y" >> target/linux/x86/config-5.4
-echo "CONFIG_NETFILTER_NETLINK_LOG=y" >> target/linux/x86/config-5.4
-echo "CONFIG_NETFILTER_XTABLES=y" >> target/linux/x86/config-5.4
-echo "CONFIG_NFT_REJECT=m" >> target/linux/x86/config-5.4
-echo "CONFIG_NFT_REJECT_IPV4=m" >> target/linux/x86/config-5.4
-echo "CONFIG_NF_CONNTRACK=y" >> target/linux/x86/config-5.4
-echo "CONFIG_NF_CONNTRACK_LABELS=y" >> target/linux/x86/config-5.4
-echo "CONFIG_NF_CT_NETLINK=y" >> target/linux/x86/config-5.4
-echo "CONFIG_NF_DEFRAG_IPV4=y" >> target/linux/x86/config-5.4
-echo "CONFIG_NF_REJECT_IPV4=m" >> target/linux/x86/config-5.4
-echo "CONFIG_NF_TABLES=y" >> target/linux/x86/config-5.4

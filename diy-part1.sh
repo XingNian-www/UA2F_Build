b6675844eddc752bca793d43a0a07bb8c42e1ce6
pxw139 <44593434+pxw139@users.noreply.github.com>
2021-05-13 11:43:22 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,20 +1,7 @@
 #!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
 
-# Hello World
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
-# Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+# 添加SSRP+
+echo "src-git helloworld https://github.com/fw876/helloworld.git" >> feeds.conf.default
 
 #添加UA2F
 git clone https://github.com/Zxilly/UA2F.git package/UA2F
